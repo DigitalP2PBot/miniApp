e975679ad53eab6d2eace5811858cc80b805d42d
Jonathan diaz <jfdiaz@playvox.com>
2025-05-10 18:00:44 -0500

    Increase gas limit

--- src/workers/transactionWorker.ts
+++ src/workers/transactionWorker.ts
@@ -9,27 +9,33 @@ const digitalP2PExchangeAbi = [
 ];
 
 enum TransactionState {
-    PENDING = "pending",
-    PROCESSING = "processing",
-    APPROVED = "approved",
-    NOT_APPROVED = "not_approved",
-    PROCCESED = "processed",
-    REJECTED = "rejected",
+  PENDING = "pending",
+  PROCESSING = "processing",
+  APPROVED = "approved",
+  NOT_APPROVED = "not_approved",
+  PROCCESED = "processed",
+  REJECTED = "rejected",
 }
 
 let hasFocus = false;
 let lastUrl = "";
 const originalOpen = window.open;
-window.open = function (url: string | URL | undefined, target: string | undefined, features: string |undefined ) {
+window.open = function (
+  url: string | URL | undefined,
+  target: string | undefined,
+  features: string | undefined,
+) {
   console.log("open", url, target, features);
   lastUrl = url as string;
   return originalOpen(url, target, features);
 };
 
 export class Transaction {
-  private transactionStatus:TransactionState = TransactionState.PENDING;
-  private usdtContract: Promise<boolean | undefined> | boolean | undefined = undefined;
-  private exchangeContract: Promise <boolean | undefined> | boolean | undefined = undefined;
+  private transactionStatus: TransactionState = TransactionState.PENDING;
+  private usdtContract: Promise<boolean | undefined> | boolean | undefined =
+    undefined;
+  private exchangeContract: Promise<boolean | undefined> | boolean | undefined =
+    undefined;
   private search: string = "";
   private walletProvider: any | null = null;
   private signer: any | null = null;
@@ -38,39 +44,53 @@ export class Transaction {
 
   constructor() {
     this.transactionStatus = TransactionState.PENDING;
-    document.addEventListener('visibilitychange', this.handleVisibilityChange);
+    document.addEventListener("visibilitychange", this.handleVisibilityChange);
   }
 
   handleVisibilityChange = () => {
     console.log(document.hasFocus());
-    hasFocus = document.visibilityState === 'visible';
-  }
+    hasFocus = document.visibilityState === "visible";
+  };
 
-  setTransactionState = (state:TransactionState) => this.transactionStatus = state;
+  setTransactionState = (state: TransactionState) =>
+    (this.transactionStatus = state);
 
   checkStatus = () => {
-    const isFinished = [TransactionState.PROCCESED, TransactionState.REJECTED, TransactionState.NOT_APPROVED].includes(this.transactionStatus)
-    if(!isFinished){
-      this.createTransaction({search: this.search, walletProvider:this.walletProvider});
+    const isFinished = [
+      TransactionState.PROCCESED,
+      TransactionState.REJECTED,
+      TransactionState.NOT_APPROVED,
+    ].includes(this.transactionStatus);
+    if (!isFinished) {
+      this.createTransaction({
+        search: this.search,
+        walletProvider: this.walletProvider,
+      });
     }
-    return {isFinished, status: this.transactionStatus, message: this.returnMessage};
-  }
+    return {
+      isFinished,
+      status: this.transactionStatus,
+      message: this.returnMessage,
+    };
+  };
 
-  public createTransaction = async ({search, walletProvider}: any) => {
+  public createTransaction = async ({ search, walletProvider }: any) => {
     this.search = search;
     this.walletProvider = walletProvider;
     const isFocused = hasFocus;
     const urlParams = new URLSearchParams(this.search);
     const signer = await this.#getSigner(this.walletProvider);
     const digitalP2PExchangeAddress = urlParams.get(
-      "networkP2pContractAddress"
+      "networkP2pContractAddress",
     ) as string;
     const cryptoAmount: number = parseFloat(
-      urlParams.get("cryptoAmount") as string
+      urlParams.get("cryptoAmount") as string,
     );
     const networkTokenAddress = urlParams.get("networkTokenAddress") as string;
-    const networkDecimals = parseInt(urlParams.get("networkDecimals") as string);
-    if(this.usdtContract === undefined) {
+    const networkDecimals = parseInt(
+      urlParams.get("networkDecimals") as string,
+    );
+    if (this.usdtContract === undefined) {
       this.usdtContract = false;
       this.#getUsdtContract({
         networkTokenAddress,
@@ -78,16 +98,18 @@ export class Transaction {
         digitalP2PExchangeAddress,
         cryptoAmount,
         networkDecimals,
-      }).then(() => {
-        this.usdtContract = true;
       })
-      .catch( e => {
-        console.error(`USDT Contract Error ${e}`);
-        this.transactionStatus = TransactionState.NOT_APPROVED;
-      });
-    };
-    const isNeedNewContract = this.exchangeContract === undefined && this.usdtContract === true;
-    if(isNeedNewContract) {
+        .then(() => {
+          this.usdtContract = true;
+        })
+        .catch((e) => {
+          console.error(`USDT Contract Error ${e}`);
+          this.transactionStatus = TransactionState.NOT_APPROVED;
+        });
+    }
+    const isNeedNewContract =
+      this.exchangeContract === undefined && this.usdtContract === true;
+    if (isNeedNewContract) {
       this.exchangeContract = false;
       const orderId: string = urlParams.get("orderId") as string;
       this.#getExchangeContract({
@@ -98,16 +120,22 @@ export class Transaction {
         cryptoAmount,
         networkDecimals,
         networkTokenAddress,
-      }).then(() => {
-        this.exchangeContract = true
-      }).catch( (e) => {
-        console.error(`Exchange Contract Error ${e}`);
-        this.transactionStatus = TransactionState.REJECTED;
-      });
+      })
+        .then(() => {
+          this.exchangeContract = true;
+        })
+        .catch((e) => {
+          console.error(`Exchange Contract Error ${e}`);
+          this.transactionStatus = TransactionState.REJECTED;
+        });
     }
-    if(isFocused && this.exchangeContract === false && this.waitForContract === undefined) {
+    if (
+      isFocused &&
+      this.exchangeContract === false &&
+      this.waitForContract === undefined
+    ) {
       this.waitForContract = setInterval(() => {
-        if(!isFocused) {
+        if (!isFocused) {
           clearInterval(this.waitForContract);
           this.waitForContract = undefined;
           return;
@@ -116,15 +144,16 @@ export class Transaction {
       }, 5000);
       this.setFocusProgrammatically();
     }
-    if (this.exchangeContract === true) this.returnMessage = "transactionApproved";
+    if (this.exchangeContract === true)
+      this.returnMessage = "transactionApproved";
     return true;
   };
 
   setFocusProgrammatically() {
     window.focus();
     window.Telegram?.WebApp?.expand();
-    window.Telegram?.WebApp?.HapticFeedback?.impactOccurred('medium');
-    const focusTarget = document.getElementById('step-pending');
+    window.Telegram?.WebApp?.HapticFeedback?.impactOccurred("medium");
+    const focusTarget = document.getElementById("step-pending");
     focusTarget?.click();
     focusTarget?.focus();
   }
@@ -133,9 +162,9 @@ export class Transaction {
     if (this.signer) return this.signer;
     this.setTransactionState(TransactionState.PROCESSING);
     const ethersProvider = new BrowserProvider(
-      walletProvider as Eip1193Provider
+      walletProvider as Eip1193Provider,
     );
-  
+
     let signer = null;
     try {
       signer = await ethersProvider.getSigner();
@@ -147,26 +176,26 @@ export class Transaction {
     this.signer = signer;
     return signer;
   };
-  
-  #getUsdtContract = async({
+
+  #getUsdtContract = async ({
     networkTokenAddress,
     signer,
     digitalP2PExchangeAddress,
     cryptoAmount,
-    networkDecimals
+    networkDecimals,
   }: any) => {
     let digitalP2PCanMoveFunds = false;
     try {
       const usdtContract = new Contract(networkTokenAddress, USDTAbi, signer);
       digitalP2PCanMoveFunds = await usdtContract
-      .approve(
-        digitalP2PExchangeAddress,
-        ethers.parseUnits(cryptoAmount.toString(), networkDecimals)
-      )
-      .then((res) => {
-        console.log("transaction approved", res);
-        return res;
-      });
+        .approve(
+          digitalP2PExchangeAddress,
+          ethers.parseUnits(cryptoAmount.toString(), networkDecimals),
+        )
+        .then((res) => {
+          console.log("transaction approved", res);
+          return res;
+        });
       this.setTransactionState(TransactionState.APPROVED);
     } catch (e: any) {
       let errorMessage = "errorApprovingTransaction";
@@ -176,8 +205,8 @@ export class Transaction {
       this.setTransactionState(TransactionState.NOT_APPROVED);
     }
     return digitalP2PCanMoveFunds;
-  }
-  
+  };
+
   #getExchangeContract = async ({
     digitalP2PCanMoveFunds,
     signer,
@@ -195,7 +224,7 @@ export class Transaction {
     const digitalP2PExchangeContract = new Contract(
       digitalP2PExchangeAddress,
       digitalP2PExchangeAbi,
-      signer
+      signer,
     );
     try {
       await digitalP2PExchangeContract
@@ -204,8 +233,8 @@ export class Transaction {
           ethers.parseUnits(cryptoAmount.toString(), networkDecimals),
           networkTokenAddress,
           {
-            gasLimit: 200000,
-          }
+            gasLimit: 300000,
+          },
         )
         .then((res) => {
           console.log("transaction processed", res);
@@ -219,4 +248,4 @@ export class Transaction {
       this.setTransactionState(TransactionState.REJECTED);
     }
   };
-};
+}
